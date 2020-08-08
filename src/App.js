fa68be5fe76996f9b49bac6ef30efaaaaf26efcc
pepino <pepino@localhost.localdomain>
2020-08-08 09:23:29 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
-import PokemonsContainer from './containers/PokemonsContainer';
+import { PokemonsContainer } from './containers/PokemonsContainer';
+import './App.css';
 
-export default function App() {
+export function App() {
     const client = new ApolloClient({
         uri: 'https://graphql-pokemon.now.sh'
     });
-
+    
     return (
-      <ApolloProvider client={client}>
-        <main>
-          <PokemonsContainer />
-        </main>
-      </ApolloProvider>
+        <ApolloProvider client={client}>
+            <main>
+                <PokemonsContainer />
+            </main>
+        </ApolloProvider>
     )
 }
