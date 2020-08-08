18fe9a1290f7cbd3dc7df9913e29b13792c0f319
pepino <pepino@localhost.localdomain>
2020-08-08 08:54:45 -0300

    graphql works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import PokemonsContainer from './containers/PokemonsContainer';
 
 export default function App() {
     const client = new ApolloClient({
@@ -7,6 +9,10 @@ export default function App() {
     });
 
     return (
-        <p>I am a Pokemon</p>
+      <ApolloProvider client={client}>
+        <main>
+          <PokemonsContainer />
+        </main>
+      </ApolloProvider>
     )
 }
---
+++ src/components/Pokemon.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export function Pokemon() {
+	return (
+		<h1>hola</h1>	
+	)
+}
+
